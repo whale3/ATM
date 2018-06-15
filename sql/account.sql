abbad2eadca091d60c8ac258155afeb250eeaf1b
whale3 <hzw.yzu@gmail.com>
2018-06-15 14:43:57 +0800

    s

--- sql/account.sql
+++
@@ -1,5 +0,0 @@
-create table account
-(id integer primary key autoincrement not null,
-name varchar(255) not null,
-password varchar(64) not null,
-balance double not null);
--- sql/log.sql
+++
@@ -1,5 +0,0 @@
-create table log
-(id integer primary key autoincrement not null,
-name varchar(255) not null,
-operation double not null,
-date timestamp not null default(datetime('now','localtime')));
